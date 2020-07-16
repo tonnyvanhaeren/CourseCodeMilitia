919f328a44a3a589c8d5d2e4720267013db6cae0
Antonius Vanhaeren <antonius.vanhaeren@@telenet.be>
2020-07-16 09:43:15 +0200

    Cruds

---
+++ web/Controllers/GroupsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        private static long currentGroupId = 1;
+        private static List<GroupViewModel> groups = new List<GroupViewModel>{
+            new GroupViewModel { Id= 1, Name= "sample group" }
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(groups);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Details(long id)
+        {
+            var group = groups.SingleOrDefault(g => g.Id == id);
+
+            if(group == null)
+            {
+                return NotFound();
+            }
+
+            return View(group);
+        }
+
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(GroupViewModel model)
+        {
+            model.Id = ++currentGroupId;
+            groups.Add(model);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("{id}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(long id, GroupViewModel model)
+        {
+            var group = groups.SingleOrDefault(g => g.Id == id);
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            group.Name = model.Name;
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ web/Models/GroupViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.Models
+{
+    public class GroupViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
