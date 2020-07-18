cf8bdcaff14d2b63026d27d71c06918870974c30
Antonius Vanhaeren <antonius.vanhaeren@@telenet.be>
2020-07-18 18:14:06 +0200

    Using logging NLOG methodes

--- web/Program.cs
+++ web/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
 
 namespace web
 {
@@ -14,15 +18,49 @@ namespace web
     {
         public static void Main(string[] args)
         {
+            ConfigureNLog();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        //TODO: replace with Nlog.config
+        private static void ConfigureNLog()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget("coloredConsole")
+            {
+                Layout = @"${date:format=HH\:mm\:ss} ${level} ${message} ${exception}"
+            };
+
+            config.AddTarget(consoleTarget);
+
+            var fileTarget = new FileTarget("file")
+            {
+                FileName = "${basedir}/file.log",
+                Layout = @"${date:format=HH\:mm\:ss} ${level} ${message} ${exception} ${ndlc}"
+            };
+
+            config.AddTarget(fileTarget);
+
+            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, consoleTarget);
+            config.AddRule(NLog.LogLevel.Trace, NLog.LogLevel.Fatal, consoleTarget, "web.ioC.*");
+            config.AddRule(NLog.LogLevel.Warn, NLog.LogLevel.Fatal, fileTarget);
+
+            LogManager.Configuration = config;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
--- web/ioC/AutofacModule.cs
+++ web/ioC/AutofacModule.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using Business.Implementation.Services;
+using Business.Models;
 using Business.Services;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,49 @@ namespace web.ioC
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
-            builder.RegisterType<InMemoryGroupsService>().As<IGroupsService>().SingleInstance();
+            builder.RegisterType<InMemoryGroupsService>().Named<IGroupsService>("groupsService").SingleInstance();
+            builder.RegisterDecorator<IGroupsService>((context, service) => new GroupsServiceDecorator(service, context.Resolve<ILogger<GroupsServiceDecorator>>()), "groupsService");
+        }
+
+        private class GroupsServiceDecorator : IGroupsService
+        {
+            private readonly ILogger<GroupsServiceDecorator> _logger;
+            private readonly IGroupsService _inner;
+
+            public GroupsServiceDecorator(IGroupsService inner, ILogger<GroupsServiceDecorator> logger)
+            {
+                _logger = logger;
+                _inner = inner;
+            }
+
+            public IReadOnlyCollection<Group> GetAll()
+            {
+                using (var scope = _logger.BeginScope("Decorator scope: {Decorator}", nameof(GroupsServiceDecorator)))
+                {
+                    _logger.LogTrace("######### Helloooooo {decoratedMethod} #########", nameof(GetAll));
+                    var result = _inner.GetAll();
+                    _logger.LogTrace("######### GoodByeeee {decoratedMethod} #########", nameof(GetAll));
+                    return result;
+                }
+            }
+
+            public Group GetById(long id)
+            {
+                _logger.LogTrace("######### Helloooooo {decoratedMethod} #########", nameof(GetById));
+                return _inner.GetById(id);
+            }
+
+            public Group Update(Group group)
+            {
+                _logger.LogTrace("######### Helloooooo {decoratedMethod} #########", nameof(Update));
+                return _inner.Update(group);
+            }
+
+            public Group Add(Group group)
+            {
+                _logger.LogWarning("######### Helloooooo {decoratedMethod} #########", nameof(Add));
+                return _inner.Add(group);
+            }
         }
     }
 }
